37f2c5641a245ea1c9c92934749d342853db1990
Adrian Berger <adrian.berger2112@gmail.com>
2017-10-01 01:02:21 +0200

    Fix mysql installation

--- check.sh
+++ check.sh
@@ -141,7 +141,8 @@ function checkMySQL () {
 function installMySQL () {
     echo "Installing MySQL..."
     sudo apt-get update
-    sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+    sudo apt-get install mysql-server
+    installPHPModule mysql
     sudo mysql_install_db
     sudo /usr/bin/mysql_secure_installation
     INSTALL_MYSQL=0
